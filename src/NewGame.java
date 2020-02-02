d0b87dac089e7f4c19b0ed82a5b0a4f6895bacfd
Rares Cristian <raresc2105@gmail.com>
2020-02-01 19:48:13 -0500

    character sheet screen

--- src/NewGame.java
+++ src/NewGame.java
@@ -73,18 +73,21 @@ public class NewGame extends Application {
     /**
      * Variables that represent character sheet
      */
+    private enum Difficulty {
+        EASY, MEDIUM, HARD;
+    };
     private static String playerName;
-    private static int difficulty;
+    private static Difficulty difficulty;
     private static int credits;
     private static int skillPoints;
 
     /**
      * Skill levels
      */
-    private int pilotSkill = 0;
-    private int fighterSkill = 0;
-    private int merchantSkill = 0;
-    private int engineerSkill = 0;
+    private static int pilotSkill = 0;
+    private static int fighterSkill = 0;
+    private static int merchantSkill = 0;
+    private static int engineerSkill = 0;
 
     /**
      * Graphics
@@ -453,6 +456,107 @@ public class NewGame extends Application {
         return pane;
     }
 
+
+    /**
+     * Character Sheet Scene
+     * @return CharacterSheet Pane
+     */
+    private static Pane characterSheet() {
+        /**
+         * Base layout
+         */
+        BorderPane pane = new BorderPane();
+        VBox vbox = new VBox();
+        HBox hbox = new HBox();
+        VBox vboxSkills = new VBox();
+        VBox vboxCredits = new VBox();
+        StackPane stackpane = new StackPane();
+   
+
+        /**
+         * Background image
+         */
+        BACKGROUND.fitWidthProperty().bind(pane.widthProperty());
+        BACKGROUND.fitHeightProperty().bind(pane.heightProperty());
+
+        /**
+         * Title
+         */
+        Text title = new Text("Welcome, " + playerName);
+        title.setFont(Font.font("Comic Sans MS", 100));
+        title.setFill(Color.YELLOW);
+
+        /* 
+         * Skills
+         */
+        Text skill = new Text("Your Skill");
+        skill.setFont(Font.font("Comic Sans MS", 75));
+        skill.setFill(Color.RED);
+
+        Text pilot = new Text("Pilot: \t\t" + pilotSkill);
+        pilot.setFont(Font.font("Comic Sans MS", 50));
+        pilot.setFill(Color.YELLOW);
+
+        Text fighter = new Text("Fighter: \t\t" + fighterSkill);
+        fighter.setFont(Font.font("Comic Sans MS", 50));
+        fighter.setFill(Color.YELLOW);
+
+        Text merchant = new Text("Fighter: \t\t" + merchantSkill);
+        merchant.setFont(Font.font("Comic Sans MS", 50));
+        merchant.setFill(Color.YELLOW);
+
+        Text engineer = new Text("Engineer: \t" + engineerSkill);
+        engineer.setFont(Font.font("Comic Sans MS", 50));
+        engineer.setFill(Color.YELLOW);
+
+        vboxSkills.getChildren().addAll(skill, pilot, fighter, merchant, engineer);
+        vboxSkills.setAlignment(Pos.TOP_CENTER);
+
+        /* 
+         * Credits
+         */
+        Text creditText = new Text("Your Credits");
+        creditText.setFont(Font.font("Comic Sans MS", 75));
+        creditText.setFill(Color.RED);
+
+        Text creditAmount = new Text(new Integer(credits).toString());
+        creditAmount.setFont(Font.font("Comic Sans MS", 50));
+        creditAmount.setFill(Color.YELLOW);
+
+        vboxCredits.getChildren().addAll(creditText, creditAmount);
+        vboxCredits.setAlignment(Pos.TOP_CENTER);
+        vboxCredits.setSpacing(100);
+
+
+        /*
+         * Difficulty
+         */
+        String difficultyText = "";
+        if (difficulty == Difficulty.EASY) {
+            difficultyText = "Easy";
+        } else if (difficulty == Difficulty.MEDIUM) {
+            difficultyText = "Medium";
+        } else {
+            difficultyText = "Hard";
+        } 
+        Text difTitle = new Text("Playing On " + difficultyText + " Mode");
+        difTitle.setFont(Font.font("Comic Sans MS", 75));
+        difTitle.setFill(Color.YELLOW);
+
+        hbox.getChildren().addAll(vboxSkills, vboxCredits);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setSpacing(100);
+
+        vbox.getChildren().addAll(title, hbox, difTitle);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(50.0); 
+
+        pane.setCenter(stackpane);
+        stackpane.getChildren().addAll(BACKGROUND, vbox);
+        return pane;
+    }
+
+
     /**
      *
      * @param primaryStage
@@ -472,6 +576,7 @@ public class NewGame extends Application {
          */
         easy.setOnAction(e -> {
             try {
+                difficulty = Difficulty.EASY;
                 credits = 5000;
                 skillPoints = 8;
                 primaryStage.setScene(new Scene(nameSelection()));
@@ -481,6 +586,7 @@ public class NewGame extends Application {
         });
         medium.setOnAction(e -> {
             try {
+                difficulty = Difficulty.MEDIUM;
                 credits = 3000;
                 skillPoints = 5;
                 primaryStage.setScene(new Scene(nameSelection()));
@@ -490,6 +596,7 @@ public class NewGame extends Application {
         });
         hard.setOnAction(e -> {
             try {
+                difficulty = Difficulty.HARD;
                 credits = 1000;
                 skillPoints = 3;
                 primaryStage.setScene(new Scene(nameSelection()));
@@ -648,9 +755,15 @@ public class NewGame extends Application {
             } catch (Throwable f) {
                 f.printStackTrace();
             }
-
         });
 
+        startGame.setOnAction(e -> {
+            try {
+                primaryStage.setScene(new Scene(characterSheet()));
+            } catch (Throwable f) {
+                f.printStackTrace();
+            }
+        });
 
 
 
